6feb676dcac9ef62c761ace6435f51fb01e8ee6a
tony-gabriel <tonyganriel@gmail.com>
2020-02-21 00:23:18 +0100

    Second commit, no much difference just linked the views in main activity.

--- app/src/main/java/com/example/fetchpro/MainActivity.java
+++ app/src/main/java/com/example/fetchpro/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.fetchpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next;
+    TextView skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = findViewById(R.id.btn_next);
+        skip = findViewById(R.id.tv_skip);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //...
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //...
+            }
+        });
     }
 }
